bf721877376b26957a488fae24d3b2522335a9d2
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-08-05 15:50:49 +0300

    Add store, plus icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.scss';
+import {useTaskStore} from './store/useTaskStore.ts';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [
+    tasks,
+    addTask,
+    updateTask,
+    removeTask,
+  ] = useTaskStore(state => [
+    state.tasks,
+    state.addTask,
+    state.updateTask,
+    state.removeTask,
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <article className={'task_list'}>
+      <h1 className={'title'}>Task List</h1>
+      <section>
+        <InputAdd />
+      </section>
 
-export default App
+      
+    </article>
+  );
+};
+
+export default App;
+
+
+// function App() {
+//   return (
+//     <>
+//       <h1 className={'title'}>Tasks</h1>
+//       <div className={'task_list'}>
+//         <TaskList />
+//       </div>
+//     </>
+//   )
+// }
